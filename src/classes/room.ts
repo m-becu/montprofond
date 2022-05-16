0f154ae9bb5efd9d1860a465c2239b2db9323a55
Anorak <contact.mbm@protonmail.com>
2022-05-16 16:13:19 +0200

    Moved room description into narration system

--- src/classes/room.ts
+++ src/classes/room.ts
@@ -60,29 +60,6 @@ export class Room {
         return this
     }
 
-    describeTo(entity: Entity) {
-
-        // Print occupants
-        let listOfOccupants: Entity[] = []
-        Object.entries(this.entities).forEach(([k, e]) => {
-            if (e.hasComponent('name') && e !== entity) listOfOccupants.push(e)
-        })
-
-        // Print exits
-        let listOfExits: IGameData[] = []
-        this.exits.forEach(e => {
-            if (e.hidden) return
-            listOfExits.push(e)
-        })
-
-        // Generate description object
-        return {
-            occupants: this.generateOccupantsString(listOfOccupants),
-            exits: this.generateExitsString(listOfExits),
-        }
-
-    }
-
     makeGameActions(game: Game, actions: IGameData[]) {
         if (!actions) return {}
         
@@ -105,43 +82,5 @@ export class Room {
     }
     
     trigger(triggerName: string) {}
-
-    generateOccupantsString(list: Entity[]) {
-        list.sort(function (a, b) {
-            let nameA = a.components.name.value.toLowerCase()
-            let nameB = b.components.name.value.toLowerCase()
-            if (nameA < nameB) return -1
-            if (nameA > nameB) return 1
-            return 0
-        })
-        let occupantsString = ""
-        if (list.length === 1) occupantsString = list[0].components.name.value
-        else if (list.length === 2) occupantsString = `${list[0].components.name.value} et ${list[1].components.name.value}`
-        else if (list.length >= 3) {
-            for (let i=0;i<list.length-1;i++) occupantsString += `${list[i].components.name.value}, `
-            occupantsString += `et ${list[list.length-1].components.name.value}`
-        }
-
-        return occupantsString === "" ? "Il n'y a personne ici." : occupantsString.length <= 1000 ? `Il y a ${occupantsString} dans cette pièce.` : "Il y a de nombreuses personnes dans cette pièce!"
-    }
-
-    generateExitsString(list: IGameData[]) {
-        list.sort(function (a, b) {
-            let nameA = a.name.toLowerCase()
-            let nameB = b.name.toLowerCase()
-            if (nameA < nameB) return -1
-            if (nameA > nameB) return 1
-            return 0
-        })
-        let exitsString = ""
-        if (list.length === 1) exitsString = list[0].desc
-        else if (list.length === 2) exitsString = `${list[0].desc} et ${list[1].desc}`
-        else if (list.length >= 3) {
-            for (let i=0;i<list.length-1;i++) exitsString += `${list[i].desc}, `
-            exitsString += `et ${list[list.length-1].desc}`
-        }
-        
-        return exitsString === "" ? "Il ne semble n'y avoir aucune sortie à cette pièce." : list.length > 1 ? `Il y a différentes sorties dans cette pièce: ${exitsString}.` : `La seule sortie semble être ${exitsString}.`
-    }
 
 }
--- src/system.ts
+++ src/system.ts
@@ -445,9 +445,35 @@ export class NarrationSystem {
     describeRoom(room: Room) {
 
         try {
+
             let componentsRow: MessageActionRow[] = []
     
-            const { occupants, exits } = room.describeTo(this.entity)
+            // Print occupants
+            let listOfOccupants: Entity[] = []
+            Object.entries(room.entities).forEach(([k, e]) => {
+                if (e.hasComponent('name') && e !== this.entity) listOfOccupants.push(e)
+            })
+
+            // Print exits
+            let listOfExits: IGameData[] = []
+            room.exits.forEach(e => {
+                if (e.hidden) {
+                    if (!this.entity.hasComponent('detection')) return
+                    else {
+                        if (e.find.dd > this.entity.components.detection.perceptionCheck) return
+                        else {
+                            console.log(`Entity ${this.entity.id} has detected a secret exit!`)
+                            listOfExits.push(e)
+                            return
+                        }
+                    }
+                }
+                listOfExits.push(e)
+            })
+
+            const occupants = this.describeOccupants(listOfOccupants)
+            const exits = this.describeExits(listOfExits)
+
             const embed = new MessageEmbed()
                 .setTitle(room.displayName)
                 .setDescription(room.desc)
@@ -468,11 +494,10 @@ export class NarrationSystem {
                 componentsRow.push(new MessageActionRow().addComponents(buttons))
             }
     
-            if (room.exits.length > 0) {
+            if (listOfExits.length > 0) {
                 // Room specific exits
                 let buttons: MessageButton[] = []
-                room.exits.forEach(a => {
-                    if (a.hidden) return
+                listOfExits.forEach(a => {
                     const button = new MessageButton()
                         .setCustomId(`exit|${a.dest}`)
                         .setLabel(a.name)
@@ -487,6 +512,39 @@ export class NarrationSystem {
     
         } catch (e) { console.error(e) }
     }
+
+    describeOccupants(list: Entity[]) {
+        list.sort(function (a, b) {
+            let nameA = a.components.name.value.toLowerCase()
+            let nameB = b.components.name.value.toLowerCase()
+            if (nameA < nameB) return -1
+            if (nameA > nameB) return 1
+            return 0
+        })
+        let occupantsString = ""
+        if (list.length === 1) occupantsString = list[0].components.name.value
+        else if (list.length === 2) occupantsString = `${list[0].components.name.value} et ${list[1].components.name.value}`
+        else if (list.length >= 3) {
+            for (let i=0;i<list.length-1;i++) occupantsString += `${list[i].components.name.value}, `
+            occupantsString += `et ${list[list.length-1].components.name.value}`
+        }
+
+        return occupantsString === "" ? "Il n'y a personne ici." : occupantsString.length <= 1000 ? `Il y a ${occupantsString} dans cette pièce.` : "Il y a de nombreuses personnes dans cette pièce!"
+    }
+
+    describeExits(list: IGameData[]) {
+        let exitsString = ""
+        if (list.length === 1) exitsString = list[0].desc
+        else if (list.length === 2) exitsString = `${list[0].desc} et ${list[1].desc}`
+        else if (list.length >= 3) {
+            for (let i=0;i<list.length-1;i++) exitsString += `${list[i].desc}, `
+            exitsString += `et ${list[list.length-1].desc}`
+        }
+        
+        return exitsString === "" ? "Il ne semble n'y avoir aucune sortie à cette pièce." : list.length > 1 ? `Il y a différentes sorties dans cette pièce: ${exitsString}.` : `La seule sortie semble être ${exitsString}.`
+    }
+
+    describeSecrets() {}
 
 }
 export class ResearchSystem {
